64ef71d9ea49287b3a344b127c99151b9e5aa48f
Nikita <n987085904@yandex.ru>
2023-04-16 16:08:34 +0300

    28. Find the Index of the First Occurrence in a String

---
+++ src/main/java/ru/mlizzie/Easy/Solution.java
@@ -0,0 +1,24 @@
+package ru.mlizzie.Easy;
+
+public class Solution {
+    /*
+    28. Find the Index of the First Occurrence in a String
+    https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/
+    * */
+    public int strStr(String haystack, String needle) {
+       if ( needle == null || haystack == null || needle.length() == 0 )
+           return -1;
+        for (int i = 0; i < haystack.length(); i++){
+           if (haystack.charAt(i) == needle.charAt(0)){
+               int j;
+               for ( j = 0; j < needle.length() && i + j < haystack.length(); j++){
+                   if(haystack.charAt(i+j) != needle.charAt(j))
+                       break;
+               }
+               if (j == needle.length() && haystack.charAt(i + j - 1) == needle.charAt(j-1))
+                   return (i);
+           }
+        }
+        return -1;
+    }
+}
---
+++ src/main/java/ru/mlizzie/Main.java
@@ -0,0 +1,13 @@
+package ru.mlizzie;
+
+import ru.mlizzie.Easy.Solution;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        Solution solution = new Solution();
+        System.out.println(solution.strStr("mississippi","issip"));
+
+    }
+}
